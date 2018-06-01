ce6516c96c23f9c629b230d117c153d929722138
Vince Martinez <eykosioux@gmail.com>
2018-06-01 10:59:11 +0100

    Use react-loadable to load components asynchronously based on routes.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,65 @@
 import React, { Component } from 'react';
 import { Hero } from 'reactbulma';
-import './App.css';
-import EmailTool from './EmailTool';
-import TweetTool from './TweetTool';
-import QuizMaker from './containers/QuizMaker';
-import ListQuizzes from './containers/ListQuizzes';
-import FundraiserMailingGenerator from './FundraiserMailingGenerator/BaseComponent';
+import Loadable from 'react-loadable';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import LoadingAnimation from './components/LoadingAnimation';
 
-import {
-  HashRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
+import './App.css';
+
+const FundraiserMailing = Loadable({
+  loading: LoadingAnimation,
+  loader: () => import('./FundraiserMailingGenerator/BaseComponent'),
+});
+const EmailTool = Loadable({
+  loading: LoadingAnimation,
+  loader: () => import('./EmailTool'),
+});
+const TweetTool = Loadable({
+  loading: LoadingAnimation,
+  loader: () => import('./TweetTool'),
+});
+const QuizMaker = Loadable({
+  loading: LoadingAnimation,
+  loader: () => import('./containers/QuizMaker'),
+});
+const ListQuizzes = Loadable({
+  loading: LoadingAnimation,
+  loader: () => import('./containers/ListQuizzes'),
+});
 
 const Routes = () => (
   <div>
-    <Router basename=''>
+    <Router basename="">
       <div>
-        <div className="navbar is-light" role="navigation" aria-label="main navigation">
+        <div
+          className="navbar is-light"
+          role="navigation"
+          aria-label="main navigation"
+        >
           <div className="navbar-brand">
-            <Link to="/" className='navbar-item'>Home</Link>
-            <Link to="/email-tool" className='navbar-item'>Email Tool</Link>
-            <Link to="/tweet-tool" className='navbar-item'>Tweet Tool</Link>
-            <Link to="/quizzes" className='navbar-item'>Quiz Tool</Link>
-            <Link to="/fundraiser-mailing" className='navbar-item'>Fundraiser Mailing Generator</Link>
+            <Link to="/" className="navbar-item">
+              Home
+            </Link>
+            <Link to="/email-tool" className="navbar-item">
+              Email Tool
+            </Link>
+            <Link to="/tweet-tool" className="navbar-item">
+              Tweet Tool
+            </Link>
+            <Link to="/quizzes" className="navbar-item">
+              Quiz Tool
+            </Link>
+            <Link to="/fundraiser-mailing" className="navbar-item">
+              Fundraiser Mailing Generator
+            </Link>
           </div>
         </div>
 
-        <Route path="/email-tool" component={EmailTool}/>
-        <Route path="/tweet-tool" component={TweetTool}/>
-        <Route path="/fundraiser-mailing" component={FundraiserMailingGenerator}/>
-        <Route path="/quiz/:id" component={QuizMaker}/>
-        <Route path="/quizzes" component={ListQuizzes}/>
+        <Route path="/email-tool" component={EmailTool} />
+        <Route path="/tweet-tool" component={TweetTool} />
+        <Route path="/fundraiser-mailing" component={FundraiserMailing} />
+        <Route path="/quiz/:id" component={QuizMaker} />
+        <Route path="/quizzes" component={ListQuizzes} />
       </div>
     </Router>
   </div>
@@ -39,12 +67,12 @@ const Routes = () => (
 
 class App extends Component {
   render() {
-    return(
+    return (
       <Hero.Head>
         <Routes />
       </Hero.Head>
     );
   }
-};
+}
 
 export default App;
---
+++ src/components/LoadingAnimation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './LoadingAnimation.css';
+
+export default () => (
+  <div className="LoadingAnimation">
+    <div className="LoadingAnimation-circle" />
+    <div className="LoadingAnimation-circle" />
+  </div>
+);
