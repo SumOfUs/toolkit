76c6c5012dbf94003efe26a4cfad94b1981c29d2
Vince Martinez <eykosioux@gmail.com>
2018-06-05 01:12:24 +0100

    Refactor CopyButton: compute on click
    Replace react-copy-to-clipboard with copy-to-clipboard. Call function to copy on click.
    This makes it a bit more responsive (instead of updating the result on every change,
    we only generate the template string when we click on copy).

---
+++ src/FundraiserMailingGenerator/components/CopyButton.js
@@ -0,0 +1,47 @@
+// @flow
+import React, { Component } from 'react';
+import { Button, Icon } from 'reactbulma';
+import copy from 'copy-to-clipboard';
+
+type Props = {
+  textFn: () => string,
+  children?: typeof React.Children,
+};
+
+type State = { hasCopied: boolean };
+
+export default class CopyButton extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      hasCopied: false,
+    };
+  }
+
+  copy = () => {
+    const text = this.props.textFn();
+    copy(text);
+    this.setState({ hasCopied: true }, () => {
+      window.setTimeout(() => this.setState({ hasCopied: false }), 3000);
+    });
+    console.log(text);
+  };
+
+  render() {
+    const { children } = this.props;
+    const { hasCopied } = this.state;
+    return (
+      <Button onClick={this.copy}>
+        <Icon small>
+          <i className="far fa-copy" />
+        </Icon>
+        {children}
+        {hasCopied ? (
+          <span style={{ color: 'red' }}>Copied</span>
+        ) : (
+          <span>Copy to clipboard</span>
+        )}
+      </Button>
+    );
+  }
+}
