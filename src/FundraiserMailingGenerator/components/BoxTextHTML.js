f7979ace120617e6b2d624c62341822f60871d5c
Vince Martínez <eyko@users.noreply.github.com>
2020-01-27 09:58:17 +0000

    wip

--- src/FundraiserMailingGenerator/components/BoxTextHTML.js
+++ src/FundraiserMailingGenerator/components/BoxTextHTML.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import InputWithActions from './InputWithActions';
 
-const DEFAULT_VALUE = 'Will you chip in {{amount}} to help ...?'
+const DEFAULT_VALUE = 'Will you chip in {{amount}} to help ...?';
 
 function BoxTextHTML(props) {
-    const [text, setText] = useState(DEFAULT_VALUE);
-    return (
-        <div id="box-text-html">
-            <InputWithActions
-                value={text}
-                onChange={(e) => {
-                    console.log('change:', e);
-                    setText(e.target.value);
-                }}
-            />
-        </div>
-    )
+  const [text, setText] = useState(DEFAULT_VALUE);
+  return (
+    <div id="box-text-html">
+      <InputWithActions
+        value={text}
+        onChange={e => {
+          console.log('change:', e);
+          setText(e.target.value);
+        }}
+      />
+    </div>
+  );
 }
 
 export default BoxTextHTML;
