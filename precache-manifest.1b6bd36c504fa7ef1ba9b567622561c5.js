07ce66bd2e1c3e1ad5da92a6b04f147a1225d059
Vince Martínez <eyko@users.noreply.github.com>
2020-01-29 10:57:38 +0000

    Updates

---
+++ precache-manifest.1b6bd36c504fa7ef1ba9b567622561c5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83463634bfe9626c56bb596263e07e7b",
+    "url": "/toolkit/index.html"
+  },
+  {
+    "revision": "3301e9ce29b718cd94a9",
+    "url": "/toolkit/static/css/5.ca025b70.chunk.css"
+  },
+  {
+    "revision": "d9f5bc16d91f017cb16d",
+    "url": "/toolkit/static/css/7.bd975875.chunk.css"
+  },
+  {
+    "revision": "53c8d3f6df8d3ac03ec8",
+    "url": "/toolkit/static/css/main.c1f1444d.chunk.css"
+  },
+  {
+    "revision": "570b895c553607b06543",
+    "url": "/toolkit/static/js/0.19348fa6.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/toolkit/static/js/0.19348fa6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "915fa009085eef7b316c",
+    "url": "/toolkit/static/js/10.93fdb4ff.chunk.js"
+  },
+  {
+    "revision": "4b3f726622c0d49a5311",
+    "url": "/toolkit/static/js/11.5e729387.chunk.js"
+  },
+  {
+    "revision": "f627f837b437d655f7d4",
+    "url": "/toolkit/static/js/3.652481d3.chunk.js"
+  },
+  {
+    "revision": "449b7ae6e7fbe264d1d5ddc7de121f5e",
+    "url": "/toolkit/static/js/3.652481d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "661a336d4720c2d7a6d4",
+    "url": "/toolkit/static/js/4.ce0645fd.chunk.js"
+  },
+  {
+    "revision": "483d86cc79bf45a097b5da82141715b1",
+    "url": "/toolkit/static/js/4.ce0645fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3301e9ce29b718cd94a9",
+    "url": "/toolkit/static/js/5.d102da2d.chunk.js"
+  },
+  {
+    "revision": "95c2173476231523b39f8e69ca391dba",
+    "url": "/toolkit/static/js/5.d102da2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d44816f3207bbeabe2f7",
+    "url": "/toolkit/static/js/6.9fc53f94.chunk.js"
+  },
+  {
+    "revision": "6e66d9057f91aa0ab8d96208fcad1702",
+    "url": "/toolkit/static/js/6.9fc53f94.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d9f5bc16d91f017cb16d",
+    "url": "/toolkit/static/js/7.b8611e38.chunk.js"
+  },
+  {
+    "revision": "0e8ff0de2ae8bc0bb4d6",
+    "url": "/toolkit/static/js/8.63fdd122.chunk.js"
+  },
+  {
+    "revision": "062d7f86496258172a34",
+    "url": "/toolkit/static/js/9.be86305a.chunk.js"
+  },
+  {
+    "revision": "53c8d3f6df8d3ac03ec8",
+    "url": "/toolkit/static/js/main.b4c232dd.chunk.js"
+  },
+  {
+    "revision": "c1ba3995cd392e47ea05",
+    "url": "/toolkit/static/js/runtime-main.b7c000b0.js"
+  }
+]);
