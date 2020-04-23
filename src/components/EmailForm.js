17fd62fbf9d5a8ca66e4dbea3f68eacfdea35753
Omar Sahyoun <osahyoun@gmail.com>
2020-04-23 17:08:00 +0100

    Fixes email form

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Form, Button, Textarea } from 'react-bulma-components';
+import { Form, Button } from 'react-bulma-components';
+import Textarea from 'react-bulma-components/lib/components/form/components/textarea';
 
 const EmailForm = props => {
   return (
     <div>
       <Form.Field>
-        <Form.Input name="to" onChange={props.handleChange} placeholder="To" />
+        <Form.Input
+          name="to"
+          onChange={props.handleChange}
+          placeholder="To"
+          value={props.to}
+        />
       </Form.Field>
       <Form.Field>
         <Form.Input
           name="subject"
+          value={props.subject}
           onChange={props.handleChange}
           placeholder="Subject"
         />
@@ -19,6 +26,7 @@ const EmailForm = props => {
           name="body"
           onChange={props.handleChange}
           placeholder="Message"
+          value={props.body}
         />
       </Form.Field>
       <Form.Field>
