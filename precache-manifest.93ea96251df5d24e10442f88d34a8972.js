bfe1aec0a1ce22e2b4eec222145be3a955443c4d
Shivashankar P <shivashankar.ror@gmail.com>
2020-04-24 15:48:55 +0530

    Updates

---
+++ precache-manifest.93ea96251df5d24e10442f88d34a8972.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf266139c1f0b0d9491c05bc89c1a87",
+    "url": "/toolkit/index.html"
+  },
+  {
+    "revision": "82b3219dc8da3aa6bdbb",
+    "url": "/toolkit/static/css/6.ca025b70.chunk.css"
+  },
+  {
+    "revision": "d5b8118a7576d115a1fc",
+    "url": "/toolkit/static/css/7.137c9787.chunk.css"
+  },
+  {
+    "revision": "d5d06becf8a879702016",
+    "url": "/toolkit/static/css/main.c1f1444d.chunk.css"
+  },
+  {
+    "revision": "d91eca0c311f65a5df84",
+    "url": "/toolkit/static/js/0.0e7dc71e.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/toolkit/static/js/0.0e7dc71e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f83d409fb0a60ed20766",
+    "url": "/toolkit/static/js/1.8584ada8.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/toolkit/static/js/1.8584ada8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d14ce70c88ea12adc578",
+    "url": "/toolkit/static/js/10.a584d975.chunk.js"
+  },
+  {
+    "revision": "ffb6d9eb7e460a4b7b3e",
+    "url": "/toolkit/static/js/11.c07a11e7.chunk.js"
+  },
+  {
+    "revision": "90834f64b14c672261c1",
+    "url": "/toolkit/static/js/12.a904c0f6.chunk.js"
+  },
+  {
+    "revision": "5ad6f813972d631a26e9",
+    "url": "/toolkit/static/js/4.cdc91407.chunk.js"
+  },
+  {
+    "revision": "449b7ae6e7fbe264d1d5ddc7de121f5e",
+    "url": "/toolkit/static/js/4.cdc91407.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75f1c36dff2eb26c59cf",
+    "url": "/toolkit/static/js/5.ecb04c7a.chunk.js"
+  },
+  {
+    "revision": "483d86cc79bf45a097b5da82141715b1",
+    "url": "/toolkit/static/js/5.ecb04c7a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "82b3219dc8da3aa6bdbb",
+    "url": "/toolkit/static/js/6.cc5316fa.chunk.js"
+  },
+  {
+    "revision": "b0c1145d4c9bf5f94bb2a2240dd0db36",
+    "url": "/toolkit/static/js/6.cc5316fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5b8118a7576d115a1fc",
+    "url": "/toolkit/static/js/7.bb6334ae.chunk.js"
+  },
+  {
+    "revision": "1ef34c9e3ed79464688e",
+    "url": "/toolkit/static/js/8.1ac1341c.chunk.js"
+  },
+  {
+    "revision": "2958f0d144b2d97b49ce",
+    "url": "/toolkit/static/js/9.e015071d.chunk.js"
+  },
+  {
+    "revision": "6e66d9057f91aa0ab8d96208fcad1702",
+    "url": "/toolkit/static/js/9.e015071d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5d06becf8a879702016",
+    "url": "/toolkit/static/js/main.42a00e24.chunk.js"
+  },
+  {
+    "revision": "f8ffdd6a2c31238b467c",
+    "url": "/toolkit/static/js/runtime-main.12143a93.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/toolkit/precache-manifest.5f93861c6bc53f8695d70dde35e01753.js"
+  "/toolkit/precache-manifest.93ea96251df5d24e10442f88d34a8972.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/toolkit/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
