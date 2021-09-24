a1f42e243028387cd0b67fe0fead178d5b14fb54
Venkatesh Ma <venkatesh@f22labs.com>
2021-09-24 15:20:21 +0530

    Updated NL translations on Locale NL.js

--- src/FundraiserMailingGenerator/locales/nl.js
+++ src/FundraiserMailingGenerator/locales/nl.js
@@ -1,6 +1,6 @@
 export const pageUrl = 'https://actions.sumofus.org/a/donate';
-export const plainText = `Helfen Sie uns mit einer Spende {{amount}} dabei?`;
-export const linkText = `Ja, ich spende {{amount}} um die Bienen zu retten.`;
+export const plainText = `Doneer jij {{amount}} om mee te helpen?`;
+export const linkText = `Ja, ik doneer {{amount}} om de bijen te redden.`;
 export const donateAnotherAmountText = `Doneer een ander bedrag`;
 export const monthlyAmount1 = 1;
 export const monthlyAmount2 = 5;
@@ -10,8 +10,7 @@ export const monthlyAmount5 = '';
 export const recurringDefault = '';
 export const amounts = [];
 export const donorButtonDefaults = `Doneer nu {{amount}}`;
-export const donorLinkDefaults = `Spenden Sie einen anderen Betrag`;
+export const donorLinkDefaults = `Doneer een ander bedrag`;
 export const nonDonorLinkDefaults = `Doneer jij {{amount}} om mee te helpen [EDIT]?`;
-export const oneClickCopy = `Als je je betaalgegevens hebt opgeslagen bij SumOfUs, wordt je donatie onmiddellijk verwerkt`;
 export const paymentMethodsCopy =
-  'Spenden dauert keine 2 Minuten - nutzen Sie einfach PayPal oder Lastschrift.';
+  'Doneren kost minder dan 2 minuten - gebruik gewoon PayPal of automatische incasso.';
